fcfe1d04e74ff5379f9cda1238b06aff4a336a52
DesyaIO <134959717+DesyaIO@users.noreply.github.com>
2025-11-03 15:40:48 +0300

    Commit LR4 ChangeToClass
    Изменение методов DFS и BFS на классы

--- LR4/main.py
+++ LR4/main.py
@@ -32,38 +32,61 @@ graph.add_edge("F","H")
 graph.add_edge("H","J")
 graph.add_edge("J","I")
 graph.add_edge("C","E")
-# ----------------------- Реализация методов ----------------
+# ----------------------- Реализация классов ----------------
 #Реализация BFS обхода - нахождения пути. (Обход в ширину)
-def shortest_path(graph,start,end):
-    queue = deque([(start,[start])])
-    visited = set()
-    while queue:
-        (node, path) = queue.popleft()
-        if node in visited:
-            continue
-        visited.add(node)
-        for child in graph.nodes[node]:
-            if child == end:
-                return path + [end]
-            else:
-                queue.append((child, path + [child]))
-    return None
+class BFSFindPath():
+    def __init__(self):
+        self.graph = 0
+        self.start = 0
+        self.end = 0
+    def set_data(self, graph, start, end):
+        self.graph = graph
+        self.start = start
+        self.end = end
+    def get_path(self):
+        queue = deque([(self.start,[self.start])])
+        visited = set()
+        while queue:
+            (node, path) = queue.popleft()
+            if node in visited:
+                continue
+            visited.add(node)
+            for child in self.graph.nodes[node]:
+                if child == self.end:
+                    return path + [self.end]
+                else:
+                    queue.append((child, path + [child]))
+        return None
 # Реализация DFS обхода - нахождения пути. (Обход в глубину)
-def dfs_find_path(graph,node,end,path):
-    if graph.nodes[node] is None:
-        return False
-    path.append(node)
-    if graph.nodes[node] == graph.nodes[end]:
-        return True
-    for child in graph.nodes[node]:
-        if dfs_find_path(graph,child,end,path):
+class DFSFindPath():
+    def __init__(self):
+        self.graph = 0
+        self.start = 0
+        self.end = 0
+    def set_data(self, graph, start, end):
+        self.graph = graph
+        self.start = start
+        self.end = end
+    def get_path(self):
+        path = []
+        if self.dfs_find_path(self.graph, self.start, self.end, path):
+            return path
+    def dfs_find_path(self,graph,node,end,path):
+        if graph.nodes[node] is None:
+            return False
+        path.append(node)
+        if graph.nodes[node] == graph.nodes[end]:
             return True
-    path.pop()
-# ------------------------------------------------------------------
-def find_path(graph,start,end):
-    path = []
-    if dfs_find_path(graph,start,end,path):
-        return path
+        for child in graph.nodes[node]:
+            if self.dfs_find_path(graph,child,end,path):
+                return True
+        path.pop()
 #--------------Вывод результатов------------------------------------
-print(find_path(graph,"A","I"))
-print(shortest_path(graph,"A","I"))
+DFS = DFSFindPath()
+BFS = BFSFindPath()
+
+DFS.set_data(graph,"A","I")
+BFS.set_data(graph,"A","I")
+
+print(F"DFS: {DFS.get_path()}")
+print(F"BFS: {BFS.get_path()}")
