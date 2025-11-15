27d46f6300e80377a14fa4656dfe7e4969b26394
DesyaIO <134959717+DesyaIO@users.noreply.github.com>
2025-11-15 11:21:10 +0300

    Delete LR2 directory

--- LR2/Scripts/Items/Potion.py
+++
@@ -1,9 +0,0 @@
-from LR2.Scripts.Items.Item import *
-from LR2.Scripts.Effect.effect import Effect
-
-
-class Potion(Item):
-    def __init__(self, effect:Effect):
-        self._effect = effect
-    def take_potion(self):
-        return self._effect
