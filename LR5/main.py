1f79bbe9fd28e1804ef2ce76404c628d45f84bd0
DesyaIO <134959717+DesyaIO@users.noreply.github.com>
2025-11-15 11:07:43 +0300

    Commit LR5

---
+++ LR5/main.py
@@ -0,0 +1,46 @@
+from string import ascii_uppercase
+alphf = '0123456789' + ascii_uppercase
+#----------------------------------------------------------
+def to_base(number,osn):
+    if number == 0: return "0"
+    result = ""
+    while number > 0:
+        result = alphf[number%osn] + result
+        number //= osn
+    return result
+#----------------------------------------------------------
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+#----------------------------------------------------------
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+#----------------------------------------------------------
+def to_pow(number,pow):
+    return number**pow
+#----------------------------------------------------------
+a = [0, 1]
+def to_fibonacci(base):
+    for i in range(2, base+1):
+        a.append(a[i-1] + a[i-2])
+    return a[base]
+#----------------------------------------------------------
+def is_simple_number(number):
+    dels = set()
+    for num in range(1, int(number**0.5) + 1):
+        if number % num == 0:
+            dels.add(num)
+            dels.add(number // num)
+    return True if len(dels) == 2 else False
+#----------------------------------------------------------
---
+++ LR5/test_main.py
@@ -0,0 +1,33 @@
+from main import to_pow, to_base, to_fibonacci, is_simple_number,bubble_sort, selection_sort
+
+def test_to_pow():
+    testNumber = 12
+    trueAnswer = 144
+    answer = to_pow(testNumber,2)
+    assert answer == trueAnswer
+def test_to_base():
+    testNumber = 120312312351
+    trueAnswer = "1C7GEM6G5"
+    answer = to_base(120312312351,23)
+    assert answer == trueAnswer
+def test_to_fibonacci():
+    testNumber = 101
+    trueAnswer = 573147844013817084101
+    answer = to_fibonacci(testNumber)
+    assert answer == trueAnswer
+def test_is_simple_number():
+    testNumber = 463
+    trueAnswer = True
+    answer = is_simple_number(testNumber)
+    assert answer == trueAnswer
+def test_bubble_sort():
+    testNumber = [45, 12, 78, 3, 91, 34, 67, 23, 89, 56, 1, 77, 42, 19, 84, 29, 61, 5, 96, 38, 72, 15, 50, 8, 99]
+    trueAnswer = [1, 3, 5, 8, 12, 15, 19, 23, 29, 34, 38, 42, 45, 50, 56, 61, 67, 72, 77, 78, 84, 89, 91, 96, 99]
+    answer = bubble_sort(testNumber)
+    assert trueAnswer == answer
+
+def test_selection_sort():
+    testNumber = [45, 12, 78, 3, 91, 34, 67, 23, 89, 56, 1, 77, 42, 19, 84, 29, 61, 5, 96, 38, 72, 15, 50, 8, 99]
+    trueAnswer = [1, 3, 5, 8, 12, 15, 19, 23, 29, 34, 38, 42, 45, 50, 56, 61, 67, 72, 77, 78, 84, 89, 91, 96, 99]
+    answer = selection_sort(testNumber)
+    assert trueAnswer == answer
