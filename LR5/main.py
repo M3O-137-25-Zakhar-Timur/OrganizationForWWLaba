e8763658a2097ab87c4cdc2345c15493e063edf6
DesyaIO <134959717+DesyaIO@users.noreply.github.com>
2025-11-15 11:17:01 +0300

    Commit LR5 Add 1 sort func

--- LR5/main.py
+++ LR5/main.py
@@ -9,24 +9,6 @@ def to_base(number,osn):
         number //= osn
     return result
 #----------------------------------------------------------
-def bubble_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        for j in range(0, n - i - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-    return arr
-#----------------------------------------------------------
-def selection_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        min_idx = i
-        for j in range(i + 1, n):
-            if arr[j] < arr[min_idx]:
-                min_idx = j
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
-    return arr
-#----------------------------------------------------------
 def to_pow(number,pow):
     return number**pow
 #----------------------------------------------------------
@@ -44,3 +26,30 @@ def is_simple_number(number):
             dels.add(number // num)
     return True if len(dels) == 2 else False
 #----------------------------------------------------------
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+#----------------------------------------------------------
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+#----------------------------------------------------------
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
--- LR5/test_main.py
+++ LR5/test_main.py
@@ -1,4 +1,4 @@
-from main import to_pow, to_base, to_fibonacci, is_simple_number,bubble_sort, selection_sort
+from main import to_pow, to_base, to_fibonacci, is_simple_number,bubble_sort, selection_sort, insertion_sort
 
 def test_to_pow():
     testNumber = 12
@@ -30,4 +30,9 @@ def test_selection_sort():
     testNumber = [45, 12, 78, 3, 91, 34, 67, 23, 89, 56, 1, 77, 42, 19, 84, 29, 61, 5, 96, 38, 72, 15, 50, 8, 99]
     trueAnswer = [1, 3, 5, 8, 12, 15, 19, 23, 29, 34, 38, 42, 45, 50, 56, 61, 67, 72, 77, 78, 84, 89, 91, 96, 99]
     answer = selection_sort(testNumber)
+    assert trueAnswer == answer
+def test_insertion_sort():
+    testNumber = [45, 12, 78, 3, 91, 34, 67, 23, 89, 56, 1, 77, 42, 19, 84, 29, 61, 5, 96, 38, 72, 15, 50, 8, 99]
+    trueAnswer = [1, 3, 5, 8, 12, 15, 19, 23, 29, 34, 38, 42, 45, 50, 56, 61, 67, 72, 77, 78, 84, 89, 91, 96, 99]
+    answer = insertion_sort(testNumber)
     assert trueAnswer == answer
